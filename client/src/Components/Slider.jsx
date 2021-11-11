abb60513cf5d8ff6f16a617cacd76a59f71924c0
abdullahalshawafi <abdullahadel.aam@gmail.com>
2021-11-11 18:21:42 +0200

    The slider is functioning now

--- client/src/Components/Slider.jsx
+++ client/src/Components/Slider.jsx
@@ -1,15 +1,37 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
 
 function Slider() {
   return (
     <div className="slider-container">
-      <div className="text-container">
-        <h1>In data we trust.</h1>
-        <p>
-          We will work with you to extract, visualize and understand you
-          building data.
-        </p>
-      </div>
+      <Carousel
+        autoPlay
+        emulateTouch
+        infiniteLoop
+        stopOnHover
+        showStatus={false}
+        axis="horizontal"
+        className="custom-carousel"
+      >
+        <div className="text-container">
+          <h1>In data we trust.</h1>
+          <p>
+            We will work with you to extract, visualize and understand you
+            building data.
+          </p>
+        </div>
+        <div className="text-container">
+          <h1>Strategic planning and execution.</h1>
+          <p>We will help you decide what not to do.</p>
+        </div>
+        <div className="text-container">
+          <h1>Software customization is the name of the game.</h1>
+          <p>
+            We can work with you to create new tools and accelerate your
+            workflows.
+          </p>
+        </div>
+      </Carousel>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
