2dbb7aaa9abf9476e474adb49e8b637ae8a20581
abdullahalshawafi <abdullahadel.aam@gmail.com>
2021-12-10 21:10:05 +0200

    Add production API in app.js

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const dotenv = require('dotenv');
 
 const db = require('./configs/mongo');
@@ -7,19 +8,28 @@ const db = require('./configs/mongo');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+const DIRNAME = __dirname.replace('server', '').slice(0, -1);
 
 app.use(cors());
 dotenv.config();
 
 db().catch(err => console.log(err));
 
+app.use(express.static(path.join(DIRNAME, 'client/build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/contact-us", require("./routes/contactUs"));
-app.use("/service", require("./routes/service"));
-app.use("/project", require("./routes/project"));
-app.use("/user", require("./routes/user"));
+app.use("/api/contact-us", require("./routes/contactUs"));
+app.use("/api/service", require("./routes/service"));
+app.use("/api/project", require("./routes/project"));
+app.use("/api/user", require("./routes/user"));
+
+if (process.env.NODE_ENV === "production") {
+    app.get('*', (req, res) => {
+        console.log(path.join(DIRNAME + '/client/build/index.html'));
+        res.sendFile(path.join(DIRNAME + '/client/build/index.html'));
+    });
+}
 
 app.listen(PORT, err => {
     if (err) return console.error(err);
