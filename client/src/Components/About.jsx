7725dfa8c7eb4836c1cb8b0f03180c1a508a39c9
abdullahalshawafi <abdullahadel.aam@gmail.com>
2021-12-24 13:57:35 +0200

    Edit some data and images

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -5,9 +5,18 @@ function About() {
 
   const aboutText = {
     mission:
-      "Bad Monkeys are out to put a dent in the AEC industry. We want to change how people construct the world around them. Our goal is to change people’s attitudes, towards sharing and collaboration so that we can all create something that will last forever.",
+      "The mission of AMC Engineering, Inc. is to provide engineering and technical solutions that are responsive to our clients' needs in an innovative, cost effective, professional, quality service manner. We will accomplish this by utilizing our common values and in achieving our business objective of having you as a satisfied client.",
     vision:
-      "We have a vision of a different AEC industry. One in which companies work together, share and support their effort in order to build a better world around us. We want to build bridges not walls.",
+      "The Company continues to move forward in become the leading multi-discipline engineering company providing project engineering, project management, engineering studies, construction and maintenance works in Egypt, by consistently delivering projects that meet international standards.",
+    values: `
+      <div>
+        Integrity - We behave ethically and are respectful, open, and honest in our business and personal lives.<br />
+        Quality Engineering - We provide high-quality engineering to meet client needs.<br />
+        Client Satisfaction - We engage our clients and the communities we serve so that they know us, trust, and value our services.<br />
+        Technical Innovation - We develop better engineering solutions through research and development.<br />
+        Employee Loyalty - We make every effort to attract and retain excellent, motivated employees, who are the source of our success.
+      </div>
+      `,
   };
 
   const handleClick = (e) => {
@@ -30,9 +39,18 @@ function About() {
           >
             vision
           </p>
+          <p
+            className={selectedAbout === "values" ? "active" : ""}
+            onClick={handleClick}
+          >
+            values
+          </p>
         </div>
       </div>
-      <p className="about-text">{aboutText[selectedAbout]}</p>
+      <p
+        className="about-text"
+        dangerouslySetInnerHTML={{ __html: aboutText[selectedAbout] }}
+      ></p>
     </div>
   );
 }
