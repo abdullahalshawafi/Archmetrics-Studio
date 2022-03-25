8ec42ff5e582dc272de7ebbe5c706cdc5a248b43
Mohamed Ahmed <mohamed.sayed001@eng-st.cu.edu.eg>
2022-03-25 18:47:48 +0200

    modify project details

--- client/src/pages/ProjectDetails.jsx
+++ client/src/pages/ProjectDetails.jsx
@@ -37,20 +37,29 @@ function ProjectDetails({ pathname, setPathname }) {
               <h4>{projectDetails.year}</h4>
             </div>
           </div>
-          <div className="project-details-container row mx-0">
-            <div className="project-details col-12 col-md-3 mb-3">
+          <div className="project-details" style={{margin: "5rem"}}>
+              <h4>Project Description:</h4>
+              <p>{projectDetails.description}</p>
+          </div>
+          <div className="project-details-container row">
+            <div className="project-details col-sm-4">
               <h4>Client:</h4>
               <p>{projectDetails.client}</p>
+              <br/>
               <h4>Location:</h4>
               <p>{projectDetails.location}</p>
+              <br/>
               <h4>Area:</h4>
               <p>
                 {projectDetails.area} m<sup>2</sup>
               </p>
+              <br/>
               <h4>Type:</h4>
               <p>{projectDetails.type}</p>
+              <br/>
               <h4>Stage:</h4>
               <p>{projectDetails.stage}</p>
+              <br/>
               <h4>Services provided:</h4>
               {projectDetails.services.map((service, index) => (
                 <Link to={`/services/${service.slug}`} key={index}>
@@ -58,16 +67,13 @@ function ProjectDetails({ pathname, setPathname }) {
                 </Link>
               ))}
             </div>
-            <div className="col-12 col-md-1"></div>
-            <div className="project-details col-12 col-md-8">
-              <h4>Project Description:</h4>
-              <p>{projectDetails.description}</p>
+            <div className="project-details col-sm-4" style={{width:"60%"}}>
               {projectDetails.images && (
                 <ImageGallery data={projectDetails.images} />
               )}
+              </div>
             </div>
           </div>
-        </div>
       )}
     </ClientLayout>
   );
