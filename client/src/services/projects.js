eae8864bc09978055985a694c12272b60204d508
abdullahalshawafi <abdullahadel.aam@gmail.com>
2022-04-22 15:07:02 +0200

    Fixed bug in edit project gallery images

--- client/src/services/projects.js
+++ client/src/services/projects.js
@@ -1,62 +1,84 @@
 import axios from "axios";
 
 export const getProjects = async (setter) => {
-    const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/project`);
-    setter(res.data.projects);
+  const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/project`);
+  setter(res.data.projects);
 };
 
 export const getSingleProject = async (project, setter, error) => {
-    try {
-        const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/project/${project}`);
-        if (res.data.project.images) {
-            res.data.project.images = res.data.project.images.map(image => ({ image }));
-        }
-        setter(res.data.project);
-    } catch (err) {
-        console.log(err.message);
-        error(true);
+  try {
+    const res = await axios.get(
+      `${process.env.REACT_APP_BASE_URL}/project/${project}`
+    );
+    if (!res.data.project) {
+      error(true);
+      return;
     }
+
+    if (res.data.project.images) {
+      res.data.project.images = res.data.project.images.map((image) => ({
+        image,
+      }));
+    }
+
+    setter(res.data.project);
+  } catch (err) {
+    console.log(err.message);
+    error(true);
+  }
 };
 
 export const createProject = async (body) => {
-    const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/project/create`, body, {
-        headers: {
-            'Content-Type': 'application/json',
-            [process.env.REACT_APP_HEADER]: localStorage.getItem('token')
-        }
-    });
+  const res = await axios.post(
+    `${process.env.REACT_APP_BASE_URL}/project/create`,
+    body,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        [process.env.REACT_APP_HEADER]: localStorage.getItem("token"),
+      },
+    }
+  );
 
-    return res.status;
+  return res.status;
 };
 
 export const editProject = async (project, body) => {
-    try {
-        body.services = body.services.map(service => service.slug);
-        body.images = body.images.map(image => image.image);
-        const res = await axios.put(`${process.env.REACT_APP_BASE_URL}/project/edit/${project}`, body, {
-            headers: {
-                'Content-Type': 'application/json',
-                [process.env.REACT_APP_HEADER]: localStorage.getItem('token')
-            }
-        });
+  try {
+    body.services = body.services.map((service) => service.slug);
+    if (body.images[0].image) {
+      body.images = body.images.map((image) => image.image);
+    }
+    const res = await axios.put(
+      `${process.env.REACT_APP_BASE_URL}/project/edit/${project}`,
+      body,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          [process.env.REACT_APP_HEADER]: localStorage.getItem("token"),
+        },
+      }
+    );
 
-        return res.status;
-    }
-    catch (err) {
-        console.log(err.message);
-        return err;
-    }
+    return res.status;
+  } catch (err) {
+    console.log(err.message);
+    return err;
+  }
 };
 
 export const deleteProject = async (projects, project, setter) => {
-    const res = await axios.delete(`${process.env.REACT_APP_BASE_URL}/project/delete/${project}`, {
-        headers: {
-            'Content-Type': 'application/json',
-            [process.env.REACT_APP_HEADER]: localStorage.getItem('token')
-        }
-    });
-    if (res.status === 200) {
-        projects = projects.filter(x => x.slug !== project);
-        setter(projects);
+  const res = await axios.delete(
+    `${process.env.REACT_APP_BASE_URL}/project/delete/${project}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        [process.env.REACT_APP_HEADER]: localStorage.getItem("token"),
+      },
     }
+  );
+  if (res.status === 200) {
+    projects = projects.filter((x) => x.slug !== project);
+    setter(projects);
+  }
 };
