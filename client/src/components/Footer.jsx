1748ee8b4703bf91d6a9a5e9f08bc4fd6dbaef98
abdullahalshawafi <abdullahadel.aam@gmail.com>
2022-04-27 18:01:39 +0200

    Added whatsapp number to the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,6 +3,7 @@ import {
   faFacebook,
   faLinkedin,
   faYoutube,
+  faWhatsapp,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../App.css";
@@ -17,6 +18,9 @@ export default function Footer() {
         </label>
       </div>
       <div className="FooterIcons">
+        <a href="https://wa.me/201008985801" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon className="Icon" icon={faWhatsapp} />
+        </a>
         <a
           href="https://www.facebook.com/AMC.Archmetrics.Studio"
           target="_blank"
@@ -24,15 +28,15 @@ export default function Footer() {
         >
           <FontAwesomeIcon className="Icon" icon={faFacebook} />
         </a>
-        <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon className="Icon" icon={faYoutube} />
-        </a>
         <a
           href="https://www.linkedin.com/company/archmetrics-amc/"
           target="_blank"
           rel="noreferrer"
         >
           <FontAwesomeIcon className="Icon" icon={faLinkedin} />
+        </a>
+        <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon className="Icon" icon={faYoutube} />
         </a>
       </div>
     </div>
