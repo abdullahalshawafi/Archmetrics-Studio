61858b46b6793d03d57759183e1db8541898e4dd
abdullahalshawafi <abdullahadel.aam@gmail.com>
2022-05-14 01:59:38 +0200

    Removed back to top button from client layout component

--- client/src/layouts/ClientLayout.jsx
+++ client/src/layouts/ClientLayout.jsx
@@ -1,21 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useMediaQuery } from "react-responsive";
 import Navbar from "../components/Navbar";
 import NavbarMobile from "../components/NavbarMobile";
 import Footer from "../components/Footer";
+import BackToTop from "../components/BackToTop";
 
 function ClientLayout({ pathname, children }) {
-  const [showButton, setShowButton] = useState(false);
   const [isNavHidden, setIsNavHidden] = useState(false);
-
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
 
   useEffect(() => {
     let prevScrollPos = window.pageYOffset;
@@ -32,8 +23,6 @@ function ClientLayout({ pathname, children }) {
       }
 
       prevScrollPos = currentScrollPos;
-
-      setShowButton(window.pageYOffset > 300 ? true : false);
     };
 
     window.addEventListener("scroll", handleWindowScroll);
@@ -55,11 +44,7 @@ function ClientLayout({ pathname, children }) {
       {children}
       <Footer />
 
-      {showButton && (
-        <div className="back-top-container" onClick={scrollToTop}>
-          <FontAwesomeIcon icon={faAngleUp} />
-        </div>
-      )}
+      <BackToTop />
     </div>
   );
 }
